5a3647371889bd010c7b9f5bf642bab826d9f9cd
Pintu95045 <131274412+Pintu95045@users.noreply.github.com>
2023-04-20 13:41:23 +0530

    Add files via upload

---
+++ src/Frame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+public class Frame extends JFrame {
+    Frame(){
+        this.setTitle("Snake Game");
+        this.add(new Panel());
+        this.setVisible(true);
+        this.setResizable(false);
+        this.pack();
+
+    }
+}
